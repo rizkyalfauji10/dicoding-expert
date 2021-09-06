bc4eaafd50bf9861e9718f22a8e71e6d8c8b1259
rizkyalfauji11 <rizkyalfauji11@gmail.com>
2021-09-06 10:54:08 +0700

    Remove cocoapods

--- Pods/Target Support Files/Pods-dicoding-expert-dicoding-expertUITests/Pods-dicoding-expert-dicoding-expertUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_dicoding_expert_dicoding_expertUITests : NSObject
-@end
-@implementation PodsDummy_Pods_dicoding_expert_dicoding_expertUITests
-@end
--- Pods/Target Support Files/Pods-dicoding-expert/Pods-dicoding-expert-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_dicoding_expert : NSObject
-@end
-@implementation PodsDummy_Pods_dicoding_expert
-@end
